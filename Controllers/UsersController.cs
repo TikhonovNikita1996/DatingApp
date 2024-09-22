6a64982cf43f815c04bab142b3f2c393a09ca8bc
Никита Тихонов <nikita.tikhonov1996@bk.ru>
2024-09-22 15:46:53 +0300

    Первый коммит

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,30 @@
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API;
+
+[ApiController]
+[Route("api/[controller]")]
+
+public class UsersController(DataContext dataContext) : ControllerBase
+{
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+    {
+        var users = await dataContext.Users.ToListAsync();
+
+        return users;
+    }
+
+    [HttpGet("{id:int}")]  // /api/users/2
+    public async Task<ActionResult<AppUser>> GetUser(int id)
+    {
+        var user = await dataContext.Users.FindAsync(id);
+
+        if (user == null) return NotFound();
+
+        return user;
+    }
+}
